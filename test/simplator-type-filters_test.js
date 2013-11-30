f0de52a36b38808b4e8ade90bd05b28e53000066
parroit <andrea.parodi@ebansoftware.net>
2013-11-30 20:03:48 +0100

    date, number, and currency

--- test/simplator-type-filters_test.js
+++ test/simplator-type-filters_test.js
@@ -15,4 +15,54 @@ describe("simplator_type_filters", function () {
         var tmpl = simplator.compile("{0 | simplatorTypeFilters}");
         expect(tmpl()).to.be.equal('simplator-type-filters ver 0.1.0');
     });
+
+    describe("date", function () {
+        it("is defined", function () {
+            var tmpl = simplator.compile("{0 | date}");
+            expect(tmpl()).to.be.equal('1970-01-01T01:00:00+01:00');
+        });
+
+        it("accept format", function () {
+            var tmpl = simplator.compile("{now | date('DD/MM/YYYY')}");
+            expect(tmpl({now:1385835352782})).to.be.equal('30/11/2013');
+        });
+    });
+
+    describe("number", function () {
+        it("is defined", function () {
+            var tmpl = simplator.compile("{n | number}");
+            expect(tmpl({n:42})).to.be.equal('42');
+        });
+
+        it("use locale for decimals", function () {
+            var tmpl = simplator.compile("{n | number(2,',')}");
+            expect(tmpl({n:4.2})).to.be.equal('4,20');
+        });
+
+        it("use locale for thowsand", function () {
+            var tmpl = simplator.compile("{n | number(0,',','.')}");
+            expect(tmpl({n:40200})).to.be.equal('40.200');
+        });
+
+    });
+
+    describe("currency", function () {
+        it("is defined", function () {
+            var tmpl = simplator.compile("{n | currency}");
+            expect(tmpl({n:42})).to.be.equal('$42.00');
+        });
+
+        it("use locale for decimals", function () {
+            var tmpl = simplator.compile("{n | currency('€',3,',')}");
+            expect(tmpl({n:4.2})).to.be.equal('€4,200');
+        });
+
+        it("use locale for thowsand", function () {
+            var tmpl = simplator.compile("{n | currency('€',2,',','.')}");
+            expect(tmpl({n:40200})).to.be.equal('€40.200,00');
+        });
+
+    });
+
+
 });
