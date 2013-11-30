31441741344d2169d45b76722490a2b950becd21
parroit <andrea.parodi@ebansoftware.net>
2013-11-30 12:28:33 +0100

    registerig

--- test/simplator-type-filters_test.js
+++ test/simplator-type-filters_test.js
@@ -2,10 +2,17 @@
 
 var expect = require("expect.js");
 var simplator_type_filters = require("../lib/simplator-type-filters");
+var simplator = require("simplator");
 
 
 describe("simplator_type_filters", function () {
     it("is defined", function () {
         expect(simplator_type_filters).to.be.an('object');
+    });
+
+
+    it("register on load", function () {
+        var tmpl = simplator.compile("{0 | simplatorTypeFilters}");
+        expect(tmpl()).to.be.equal('simplator-type-filters ver 0.1.0');
     });
 });
